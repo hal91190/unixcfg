793aa2e49da6adb82b75d5506606d9e2685dc959
Stéphane Lopes <stephane.lopes@prism.uvsq.fr>
2014-12-26 15:39:54 +0100

    feat(setup): ajoute un script d'installation

---
+++ setup.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Debug
+# set -xv
+
+CFG_DIR=~/configs
+
+function install_cfg_file {
+	DEST=~/$1
+	if [ -h $DEST -o ! -e $DEST ]; then
+		[ -e $DEST ] && unlink $DEST
+		ln -s $CFG_DIR/$1 $DEST
+	else
+		echo "$DEST is not a symlink."
+	fi
+}
+
+install_cfg_file .minttyrc
+
+install_cfg_file .bash_aliases
+install_cfg_file .bashrc
