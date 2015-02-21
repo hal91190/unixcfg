e35ce558ff68d7e504e690e38bcd98fb84e158f0
Stéphane Lopes <stephane.lopes@prism.uvsq.fr>
2015-02-21 20:57:59 +0100

    feat(vim): remplace gedit par gvim au niveau de gnome

--- setup.bash
+++ setup.bash
@@ -6,6 +6,21 @@
 CFG_DIR=~/configs
 
 # crée un lien symbolique pour un fichier de config
+# #1 source du lien
+# #2 lien à créer
+function create_link {
+	SRC=$CFG_DIR/$1
+	DEST=$2
+	if [ -h $DEST -o ! -e $DEST ]; then
+		[ -h $DEST ] && unlink $DEST
+		echo "Création du lien symbolique $DEST -> $SRC."
+		ln -s $SRC $DEST
+	else
+		echo "$DEST n'est pas un lien symbolique."
+	fi
+}
+
+# crée un lien symbolique pour un fichier de config
 # #1 fichier à installer
 #    si le nom du fichier débute par un _, il sera remplacé par un .
 function install_cfg_file {
@@ -14,13 +29,7 @@ function install_cfg_file {
 	else
 		DEST=~/$1
 	fi
-	if [ -h $DEST -o ! -e $DEST ]; then
-		[ -h $DEST ] && unlink $DEST
-		echo "Création du lien symbolique $DEST -> $CFG_DIR/$1."
-		ln -s $CFG_DIR/$1 $DEST
-	else
-		echo "$DEST n'est pas un lien symbolique."
-	fi
+    create_link $1 $DEST
 }
 
 # Le terminal sur Mac OS X
@@ -33,8 +42,11 @@ install_cfg_file _bashrc
 
 # Git
 install_cfg_file _gitconfig
-ln -s $CFG_DIR/_vim ~/.vim
+create_link _vim ~/.vim
 
 # ViM
 install_cfg_file _vimrc
+
+# Applications par défaut sous Gnome
+create_link defaults.list ~/.local/share/applications/defaults.list
 
